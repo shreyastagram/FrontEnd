be1248a2ac3754fadfcdd89c4958ebbe09b5a5f9
shreyastagram <124891362+shreyastagram@users.noreply.github.com>
2024-10-29 00:41:28 +0530

    created home page for project management

---
+++ FrontEnd/JAvaScript/index.js
@@ -0,0 +1,34 @@
+var library={
+    shelf1:{
+        genre:"Science",
+        categories:[
+            {name:"Science fiction", id:1 },
+            {name :"Physics", id:2},
+            {name:"Physics",id:3}
+        ]
+    },
+    shelf2:{
+        genre:"Classics",
+        categories:[
+            {name:"Tolstoy",id:4},
+            {name:"Chekov",id:5},
+            {name:"Astronomy",id:6}
+        ]
+    },
+    shelf3:{
+        genre:"Thriller",
+        categories:[
+            {name:"Sherlock Holmes",id:7},
+            {name:"Hercule Poirot",id:8},
+            {name:"James Headley",id:9},
+        ]
+    },
+    shelf4:{
+        genre:"Fantasy",
+        categories:[
+            {name:"Harry Potter",id:10},
+            {name:"Lord of the Rings",id:11},
+            {name:"A song of ice and fire",id:12}
+        ]
+    }
+};
